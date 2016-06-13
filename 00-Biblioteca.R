27e7dd5bfd8092fab625441e420029c8ae3ae1af
GabrielEstatistica <gabriel.fg1@hotmail.com>
2016-06-13 17:53:44 -0300

    Adicionando os arquivos R e Rmd.

---
+++ 00-Biblioteca.R
@@ -0,0 +1,63 @@
+
+source("./auxBib.R")
+
+# endereco <- file.choose()
+dados <- read.csv("./SistemaBibliotecasUFC_alunos.csv", header = TRUE, 
+                  sep = ",", dec = ".", encoding = "UTF-8")  # ler o arquivo
+
+Gerar_Matriz_Ajustada <- function(dados, salvar = TRUE) {
+  
+  
+  # criando uma matriz Geral para todas as vari?veis
+  
+  j <- 1
+#  multitem <- Gerar_matriz_binaria(dados[, 1], j)$Multitem
+#  nlevel <- Gerar_matriz_binaria(dados[, 1], j)$N
+  matriz <- Gerar_matriz_binaria(dados[, 1], j, dados)$Matriz    # primeiro ? feita a primeira coluna
+  lista <- list(matriz)                                   # lista para conter a matriz das questões separadas
+  for (i in 2:ncol(dados)) {                # depois as demais colunas, pois uso o cbind para criar a matriz final
+#    multitem <- c(multitem, Gerar_matriz_binaria(dados[, i], j)$Multitem)
+#    nlevel <- c(nlevel, Gerar_matriz_binaria(dados[, i], j)$N)
+    j <- j + 1
+    matrizi <- Gerar_matriz_binaria(dados[, i], j, dados)$Matriz
+    lista <- c(lista, list(matrizi))
+    matriz <- cbind(matriz, matrizi)
+    
+  }
+  
+  names(lista) <- colnames(dados)       # nomeando os elementos da lista
+  
+ # ncolunas_var <- nlevel
+ # if(all(multitem == 0)) ncolunas_var <- rep(1, length(nlevel)) else ncolunas_var[which(multitem == 0)] <- 1
+  
+  # preparando a sa?da em xls
+  Resultado <- cbind(matriz, rep("", nrow(dados)), c(paste0("V", 1:ncol(dados)), rep("", (nrow(dados) - ncol(dados)))), 
+                     c(colnames(dados), rep("",(nrow(dados) - ncol(dados)))))
+  # salvando em xls
+  if(salvar == TRUE){
+    write.table(Resultado, file = "./Matrix_Ajustada.csv", sep = ";",row.names = FALSE, col.names = TRUE)
+  }
+  
+  # frequ?ncia e percentual
+  freq_aux <- function(x){
+    if(is.numeric(x)){
+      aux <- cbind(colSums(x),round(colSums(x)/nrow(dados)*100,1))
+      colnames(aux) <- c("Frequ?ncia","%")
+    }else{
+      aux <- cbind(table(x), round(table(x)/nrow(dados)*100,1))
+      colnames(aux) <- c("Frequ?ncia","%")
+    }
+    return(aux)
+  }
+  frequencia <- lapply(lista, freq_aux)
+  
+  invisible(list(Matriz = matriz,  Lista = lista, Frequencia = frequencia))
+}
+
+## s? para as quest?es 2 e 9
+a <- Gerar_Matriz_Ajustada(dados,salvar = TRUE)
+
+
+
+a
+# for(i in nitens) {print(a$Tabela[j:i+j-1,]);j<-i+1}
